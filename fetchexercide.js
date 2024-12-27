6613a2c79fdc37f9938c17ec0f776e602addf02f
Manideep <kunisettymanideep69@gmail.com>
2024-12-27 01:55:14 -0600

    updated fetch

---
+++ fetchexercide.js
@@ -0,0 +1,17 @@
+// URL to fetch data from
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
+
+// Fetch data from the URL
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
